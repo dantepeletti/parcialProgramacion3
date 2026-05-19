d8921154d0df029376f0cae179e208f82f76e0ae
Dante <dantepeletti@gmail.com>
2026-05-18 22:58:00 -0300

    feat: estilos de carrito agregados, y algunas correcciones

--- proteger_rutas/src/pages/store/cart/cart.ts
+++ proteger_rutas/src/pages/store/cart/cart.ts
@@ -11,8 +11,11 @@ const carrito = obtenerCarrito()
 if(contenerdor){
     carrito.forEach(producto => {
         contenerdor.innerHTML += `
-        <div>
+        <div class="card-carrito">
             <h3>${producto.nombre}</h3>
+            <img 
+            src="../../../../public/pizza.jpg"
+            alt="${producto.nombre}">
             <p>Precio: $${producto.precio}</p>
             <p>Cantidad: ${producto.cantidad}</p>
         </div>
